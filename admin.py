e2ecde7ee1d8c301b0b5f0c88c93d6e218c959a3
Selma-99 <72182677+Selma-99@users.noreply.github.com>
2021-06-27 13:55:11 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,118 +1,145 @@
 from flask import *
 from database import *
+import uuid
 
 admin=Blueprint('admin',__name__)
 
-@admin.route("/admin_home")
+@admin.route('/admin_home')
 def admin_home():
-	return render_template("admin_home.html")
-
-@admin.route("/admin_manage_doctor",methods=['get','post'])
-def admin_manage_doctor():
-	data={}
-	q="SELECT * FROM `hospitals`"
-	data['hospital']=select(q)
-	q="SELECT * FROM `hospitals` inner join doctors using(hospital_id)"
-	data['doctor_view']=select(q)
-	if 'action' in request.args:
-		action=request.args['action']
-		did=request.args['did']
-
+	if not session.get("lid") is None:
+		return render_template("admin_home.html")
 	else:
-		action=None
-	if action=='delete':
-		q="delete from doctors where login_id='%s'"%(did)
-		delete(q)
-		q="delete from login where login_id="'%s'%(did)
-		delete(q)
-		return redirect(url_for('admin.admin_manage_doctor'))
-	if action=='update':
-		q="select * from doctors inner join hospitals using(hospital_id) where login_id='%s'"%(did)
-		data['updatess']=select(q)
+		return redirect(url_for("public.login"))
 
-	if 'submits' in request.form:
-		hosp=request.form['hosp']
-		dept=request.form['dept']
-		dname=request.form['dname']
-		phone=request.form['phone']
-		email=request.form['email']
-		
-		q="update `doctors` set `hospital_id`='%s',`dept`='%s',`dname`='%s',`phone`='%s',`email`='%s' where login_id='%s'"%(hosp,dept,dname,phone,email,did)
-		update(q)
-		return redirect(url_for("admin.admin_manage_doctor"))
-	if 'submit' in request.form:
-		hosp=request.form['hosp']
-		dept=request.form['dept']
-		dname=request.form['dname']
-		phone=request.form['phone']
-		email=request.form['email']
-		q="INSERT INTO `login`(`username`,`password`,`usertype`) VALUES('%s','%s','doctor')"%(email,phone)
-		lid=insert(q)
-		q="INSERT INTO `doctors`(`login_id`,`hospital_id`,`dept`,`dname`,`phone`,`email`) VALUES('%s','%s','%s','%s','%s','%s')"%(lid,hosp,dept,dname,phone,email)
-		insert(q)
-		return redirect(url_for("admin.admin_manage_doctor"))
 
-	return render_template("admin_manage_doctor.html",data=data)	
-
-@admin.route("/admin_manage_hospital",methods=['get','post'])
+@admin.route('/admin_manage_hospital',methods=['get','post'])
 def admin_manage_hospital():
-	data={}
-	if 'action' in request.args:
-		action=request.args['action']
-		hid=request.args['hid']
-
-	else:
-		action=None
-	if action=='delete':
-		q="delete from hospitals where hospital_id='%s'"%(hid)
-		delete(q)
-		
-		return redirect(url_for('admin.admin_manage_hospital'))
-
-	if action=='update':
-		q="select * from hospitals where hospital_id='%s'"%(hid)
-		data['updatess']=select(q)
+	if not session.get("lid") is None:
+		data={}
+		q="SELECT * FROM `hospitals`"
+		data['hospital']=select(q)
+		if 'action' in request.args:
+			action=request.args['action']
+			hid=request.args['hid']
+		else:
+			action=None
+		if action=='delete':
+			q="delete from hospitals where hospital_id='%s'"%(hid)
+			delete(q)
+			flash('deleted...')
+			return redirect(url_for('admin.admin_manage_hospital'))
+		if action=='update':
+			q="select * from hospitals where hospital_id='%s'"%(hid)
+			print(q)
+			data['updatess']=select(q)
 		if 'submits' in request.form:
-
-			hospital=request.form['hospital']
+			hname=request.form['hname']
 			place=request.form['place']
 			phone=request.form['phone']
-			q="update `hospitals` set `hospital`='%s',`place`='%s',`phone`='%s' where hospital_id='%s'"%(hospital,place,phone,hid)
+			q="UPDATE `hospitals` SET `hospital`='%s',`place`='%s',`phone`='%s' where hospital_id='%s'"%(hname,place,phone,hid)
 			update(q)
-			return redirect(url_for("admin.admin_manage_hospital"))
+			flash('Updated Successfully...')
+			return redirect(url_for('admin.admin_manage_hospital'))
+
+		if 'submit' in request.form:
+			hname=request.form['hname']
+			place=request.form['place']
+			phone=request.form['phone']
+			q="INSERT INTO `hospitals`(`hospital`,`place`,`phone`) VALUES('%s','%s','%s')"%(hname,place,phone)
+			insert(q)
+			flash('Registered Successfully...')
+			return redirect(url_for('admin.admin_manage_hospital'))
+		return render_template("admin_manage_hospital.html",data=data)
+	else:
+		return redirect(url_for("public.login"))
 
 
+@admin.route('/admin_manage_doctor',methods=['get','post'])
+def admin_manage_doctor():
+	if not session.get("lid") is None:
+		data={}
+		q="SELECT *,doctors.phone as dphone,doctors.email as demail FROM `doctors` INNER JOIN `hospitals` USING(`hospital_id`)"
+		data['doctor']=select(q)
+		q="select * from hospitals"
+		data['hospital']=select(q)
+		if 'action' in request.args:
+			action=request.args['action']
+			did=request.args['did']
+		else:
+			action=None
+		if action=='delete':
+			q="delete from doctors where doctor_id='%s'"%(did)
+			delete(q)
+			flash('deleted...')
+			return redirect(url_for('admin.admin_manage_doctor'))
+		if action=='update':
+			q="SELECT * FROM `doctors` INNER JOIN `hospitals` USING(`hospital_id`) where doctor_id='%s'"%(did)
+			print(q)
+			data['updatess']=select(q)
+		if 'submits' in request.form:
+			hospiatl=request.form['hospiatl']
+			dname=request.form['dname']
+			phone=request.form['phone']
+			email=request.form['email']
 
-	data['selma']=2
-	q="SELECT * FROM hospitals"
-	data['hospital']=select(q)
-	if 'submit' in request.form:
-		hname=request.form['hname']
-		place=request.form['place']
-		phone=request.form['phone']
+			q="UPDATE `doctors` SET `hospital_id`='%s',`doctor_name`='%s',`phone`='%s',`email`='%s' WHERE `doctor_id`='%s'"%(hospiatl,dname,phone,email,did)
+			update(q)
+			flash('Updated Successfully...')
+			return redirect(url_for('admin.admin_manage_doctor'))
 
-		q="INSERT INTO `hospitals`(`hospital`,`place`,`phone`) VALUES('%s','%s','%s')"%(hname,place,phone)
-		insert(q)
-		return redirect(url_for("admin.admin_manage_hospital"))
+		if 'submit' in request.form:
+			hospiatl=request.form['hospiatl']
+			dname=request.form['dname']
+			phone=request.form['phone']
+			email=request.form['email']
+			uname=request.form['uname']
+			pwd=request.form['pwd']
+			q="INSERT INTO `login`(`username`,`password`,`usertype`) VALUES('%s','%s','doctor')"%(uname,pwd)
+			lid=insert(q)
+			q="INSERT INTO `doctors`(login_id,`hospital_id`,`doctor_name`,`phone`,`email`) VALUES('%s','%s','%s','%s','%s')"%(lid,hospiatl,dname,phone,email)
+			insert(q)
+			flash('Registered Successfully...')
+			return redirect(url_for('admin.admin_manage_doctor'))
+		return render_template("admin_manage_doctor.html",data=data)
+	else:
+		return redirect(url_for("public.login"))
 
-	return render_template("admin_manage_hospital.html",data=data)	
 
-@admin.route("/admin_view_patient")
+@admin.route('/admin_view_patient',methods=['get','post'])
 def admin_view_patient():
-	data={}
-	q="SELECT * FROM `patient`"
-	data['patient']=select(q)
-	return render_template("admin_view_patient.html",data=data)	
+	if not session.get("lid") is None:
+		data={}
+		q="SELECT * FROM `patient`"
+		data['patient']=select(q)
+		
 
-@admin.route("/admin_view_patient_precaution")
-def admin_view_patient_precaution():
-	data={}
-	q="SELECT * FROM `precaution`"
-	data['precaution']=select(q)
-	return render_template("admin_view_patient_precaution.html",data=data)	
+		return render_template("admin_view_patient.html",data=data)
+	else:
+		return redirect(url_for("public.login"))
 
-	
 
-@admin.route("/admin_filesuploaded")
-def admin_filesuploaded():
-	return render_template("admin_filesuploaded.html")	
+
+@admin.route('/admin_view_files',methods=['get','post'])
+def admin_view_files():
+	if not session.get("lid") is None:
+		data={}
+		pid=request.args['pid']
+		q="SELECT *,`doctors`.`doctor_name` AS dname FROM `files` INNER JOIN `doctors` USING(`doctor_id`) INNER JOIN `precaution` USING(`file_id`) WHERE `patient_id`='%s'"%(pid)
+		print(q)
+		data['files']=select(q)
+		
+
+		return render_template("admin_view_files.html",data=data)
+	else:
+		return redirect(url_for("public.login"))
+
+
+@admin.route('/admin_view_rating')
+def admin_view_rating():
+	if not session.get("lid") is None:
+		data={}
+		q="SELECT * FROM `rate` INNER JOIN `patient` ON(`patient`.`patient_id`=`rate`.`user_id`)"
+		data['rating']=select(q)
+		return render_template("admin_view_rating.html",data=data)
+	else:
+		return redirect(url_for("public.login"))
